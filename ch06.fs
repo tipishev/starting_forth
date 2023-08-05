8fc337c49a1c18af55b2c3342f0377be14a74119
Timofey Tipishev <timofey.tipishev@klarna.com>
2023-08-06 00:32:00 +0300

    add ch06ex08 solution

--- ch06.fs
+++ ch06.fs
@@ -46,6 +46,8 @@
 ( ex 4 ) : /stars ( n -- ) 0 swap do cr i spaces 10 stars -1 +loop ;
 ( ex 5 ) : /stars2 ( n -- ) begin cr dup spaces 10 stars 1 - dup 0= until drop ;
 ( ex 6, I gave up and copied the solution for studying :-/ )
-
 : triangle ( increment limit start -- ) do cr 9 i - spaces i 2* 1+ stars dup +loop drop ;
 : diamonds ( #diamonds -- ) 0 ?do 1 10 0 triangle -1 0 9 triangle loop ;
+( ex 7 skipped, not interested )
+( ex 8 ) : ** ( n exp -- n**exp ) 1 swap 0 do over * loop swap drop ;
+
