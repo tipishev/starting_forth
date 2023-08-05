b9dee321a5677dd1e402ec16c9143da91e7a02d5
Timofey Tipishev <timofey.tipishev@klarna.com>
2023-08-05 23:37:16 +0300

    add ch06 exercise solutions

--- ch06.fs
+++ ch06.fs
@@ -35,4 +35,17 @@
 : inc-count ( increment limit index -- )
     do i . dup +loop drop ;
 
-: TEST  100 10 DO  I . -1 +LOOP ;
+\ : TEST  100 10 DO  I . -1 +LOOP ;
+
+( exercises )
+
+: star [char] * emit ;
+( ex 1 ) : stars ( #stars -- ) 0 ?do star loop ;
+( ex 2 ) : box ( width height -- ) 0 do cr dup stars loop ;
+( ex 3 ) : \stars ( n -- ) 0 do cr i spaces 10 stars loop ;
+( ex 4 ) : /stars ( n -- ) 0 swap do cr i spaces 10 stars -1 +loop ;
+( ex 5 ) : /stars2 ( n -- ) begin cr dup spaces 10 stars 1 - dup 0= until drop ;
+( ex 6, I gave up and copied the solution for studying :-/ )
+
+: triangle ( increment limit start -- ) do cr 9 i - spaces i 2* 1+ stars dup +loop drop ;
+: diamonds ( #diamonds -- ) 0 ?do 1 10 0 triangle -1 0 9 triangle loop ;
